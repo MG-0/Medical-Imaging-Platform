376bbaac17496be81cd72dcffb4447d4226a551f
Mahmoud Gamal <gamalgit@gmail.com>
2026-05-10 01:31:19 +0300

    feat: Add Docker and Kubernetes configuration

--- Ai_Services/app.py
+++ Ai_Services/app.py
@@ -96,4 +96,4 @@ def predict():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
